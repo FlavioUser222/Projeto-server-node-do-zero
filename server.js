33764ebb07474f543c2d6ac1f53f475e724d3a73
FlavioUser222 <flavinhoth12@gmail.com>
2025-02-28 16:15:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -33,6 +33,41 @@ server.get('/notas',(request,reply)=>{
     ]
    reply.send(notas)
 })
+server.get('/livros',(request,reply)=>{
+    let livros = [
+       {
+        foto: "https://m.media-amazon.com/images/I/71M-fsD472L._AC_UF1000,1000_QL80_.jpg",
+        nome: "As Seis Lições",
+        desconto: "50%",
+        valor: 100,
+    },
+    {
+        foto: "https://m.media-amazon.com/images/I/71M-fsD472L._AC_UF1000,1000_QL80_.jpg",
+        nome: "As Seis Lições",
+        desconto: "20%",
+        valor: 100,
+    },
+    {
+        foto: "https://m.media-amazon.com/images/I/71M-fsD472L._AC_UF1000,1000_QL80_.jpg",
+        nome: "As Seis Lições",
+        desconto: "15%",
+        valor: 100,
+    },
+    {
+        foto: "https://m.media-amazon.com/images/I/71M-fsD472L._AC_UF1000,1000_QL80_.jpg",
+        nome: "As Seis Lições",
+        desconto: "65%",
+        valor: 100,
+    },
+    {
+        foto: "https://m.media-amazon.com/images/I/71M-fsD472L._AC_UF1000,1000_QL80_.jpg",
+        nome: "As Seis Lições",
+        desconto: "5%",
+        valor: 100,
+    },
+    ]
+   reply.send(livros)
+})
 
 
 // server.post('/chamada')
